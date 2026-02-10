334a535b91f687d51721bf00c00cfb45e518852a
Daniel Fisseha <developedbydanielfisseha@gmail.com>
2026-02-10 22:15:06 +0300

    fix: fixed the playroom logic

--- card-rpg-frontend/src/main.tsx
+++ card-rpg-frontend/src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 insertCoin({
   streamMode: false, // Disable stream mode to allow symmetric multiplayer (each peer is a player)
-  skipLobby: true, // Optional: Skip lobby for faster dev, but usually you want lobby to invite friends
+  // skipLobby: true,  // Optional: Skip lobby for faster dev, but usually you want lobby to invite friends
 }).then(() => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
