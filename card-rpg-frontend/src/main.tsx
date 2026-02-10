1ba84a32f32da18b24d6c4892ed265c6380f1692
Daniel Fisseha <developedbydanielfisseha@gmail.com>
2026-02-10 22:10:33 +0300

    feat(main): initialize PlayroomKit

--- card-rpg-frontend/src/main.tsx
+++ card-rpg-frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { insertCoin } from "playroomkit";
+import { GameEngineProvider } from "./hooks/useGameEngine";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+insertCoin({
+  streamMode: true, // Optional: Enable if you want to support stream mode (e.g. for OBS)
+}).then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <GameEngineProvider>
+        <App />
+      </GameEngineProvider>
+    </React.StrictMode>,
+  );
+});
