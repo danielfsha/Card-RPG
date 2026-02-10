bbefa764deda9197b333ddb8a2a6c5aa5f1ead61
Daniel Fisseha <developedbydanielfisseha@gmail.com>
2026-02-10 21:47:51 +0300

    fix: updated the index.css to remove any extra code

--- card-rpg-frontend/src/App.tsx
+++ card-rpg-frontend/src/App.tsx
@@ -2,7 +2,7 @@ import Experience from "./components/Experience";
 
 export default function App() {
   return (
-    <div className="w-screen h-screen overflow-hidden">
+    <div className="w-screen h-screen overflow-hidden bg-amber-500">
       <Experience />
     </div>
   );
